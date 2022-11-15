a11b3f81a5ab8fcd2fd1649e2da29ba48654e4bc
YujinChang <yujinchang804@ewhain.net>
2022-11-15 20:48:29 +0900

    [1115] sql file 수정

--- sql_files/create.sql
+++ sql_files/create.sql
@@ -11,10 +11,10 @@ use team19;
 CREATE TABLE IF NOT EXISTS movies_ott(
     mid INTEGER, netflix INTEGER,amazon_prime INTEGER, disney_plus INTEGER, hulu INTEGER,
     original_title VARCHAR(100),homepage VARCHAR(100), overview TEXT,
-    popularity FLOAT, production_countries VARCHAR(20), release_date DATE,
-    revenue INTEGER, runtime FLOAT, spoken_languages VARCHAR(20), vote_average FLOAT, vote_count INTEGER,
-    genres_1 VARCHAR(20), genres_2 VARCHAR(20), genres_3 VARCHAR(20), 
-    keywords_1 VARCHAR(20), keywords_2 VARCHAR(20), keywords_3 VARCHAR(20)
+    popularity FLOAT, production_countries VARCHAR(200), release_date DATE,
+    revenue BIGINT, runtime FLOAT, spoken_languages VARCHAR(200), vote_average FLOAT, vote_count INTEGER,
+    genres_1 VARCHAR(200), genres_2 VARCHAR(200), genres_3 VARCHAR(200), 
+    keywords_1 VARCHAR(200), keywords_2 VARCHAR(200), keywords_3 VARCHAR(200)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
@@ -22,40 +22,40 @@ CREATE TABLE IF NOT EXISTS movies_ott(
 CREATE TABLE IF NOT EXISTS movies_netflix(
     mid INTEGER,
     original_title VARCHAR(100),homepage VARCHAR(100), overview TEXT,
-    popularity FLOAT, production_countries VARCHAR(20), release_date DATE,
-    revenue INTEGER, runtime FLOAT, spoken_languages VARCHAR(20), vote_average FLOAT, vote_count INTEGER,
-    genres_1 VARCHAR(20), genres_2 VARCHAR(20), genres_3 VARCHAR(20), 
-    keywords_1 VARCHAR(20), keywords_2 VARCHAR(20), keywords_3 VARCHAR(20)
+    popularity FLOAT, production_countries VARCHAR(200), release_date DATE,
+    revenue BIGINT, runtime FLOAT, spoken_languages VARCHAR(200), vote_average FLOAT, vote_count INTEGER,
+    genres_1 VARCHAR(200), genres_2 VARCHAR(200), genres_3 VARCHAR(200), 
+    keywords_1 VARCHAR(200), keywords_2 VARCHAR(200), keywords_3 VARCHAR(200)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- amazon_prime
 CREATE TABLE IF NOT EXISTS movies_amazon_prime(
     mid INTEGER,
     original_title VARCHAR(100),homepage VARCHAR(100), overview TEXT,
-    popularity FLOAT, production_countries VARCHAR(20), release_date DATE,
-    revenue INTEGER, runtime FLOAT, spoken_languages VARCHAR(20), vote_average FLOAT, vote_count INTEGER,
-    genres_1 VARCHAR(20), genres_2 VARCHAR(20), genres_3 VARCHAR(20), 
-    keywords_1 VARCHAR(20), keywords_2 VARCHAR(20), keywords_3 VARCHAR(20)
+    popularity FLOAT, production_countries VARCHAR(200), release_date DATE,
+    revenue BIGINT, runtime FLOAT, spoken_languages VARCHAR(200), vote_average FLOAT, vote_count INTEGER,
+    genres_1 VARCHAR(200), genres_2 VARCHAR(200), genres_3 VARCHAR(200), 
+    keywords_1 VARCHAR(200), keywords_2 VARCHAR(200), keywords_3 VARCHAR(200)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- disney_plus
 CREATE TABLE IF NOT EXISTS movies_disney_plus(
     mid INTEGER,
     original_title VARCHAR(100),homepage VARCHAR(100), overview TEXT,
-    popularity FLOAT, production_countries VARCHAR(20), release_date DATE,
-    revenue INTEGER, runtime FLOAT, spoken_languages VARCHAR(20), vote_average FLOAT, vote_count INTEGER,
-    genres_1 VARCHAR(20), genres_2 VARCHAR(20), genres_3 VARCHAR(20), 
-    keywords_1 VARCHAR(20), keywords_2 VARCHAR(20), keywords_3 VARCHAR(20)
+    popularity FLOAT, production_countries VARCHAR(200), release_date DATE,
+    revenue BIGINT, runtime FLOAT, spoken_languages VARCHAR(200), vote_average FLOAT, vote_count INTEGER,
+    genres_1 VARCHAR(200), genres_2 VARCHAR(200), genres_3 VARCHAR(200), 
+    keywords_1 VARCHAR(200), keywords_2 VARCHAR(200), keywords_3 VARCHAR(200)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- hulu
 CREATE TABLE IF NOT EXISTS movies_hulu(
     mid INTEGER,
     original_title VARCHAR(100),homepage VARCHAR(100), overview TEXT,
-    popularity FLOAT, production_countries VARCHAR(20), release_date DATE,
-    revenue INTEGER, runtime FLOAT, spoken_languages VARCHAR(20), vote_average FLOAT, vote_count INTEGER,
-    genres_1 VARCHAR(20), genres_2 VARCHAR(20), genres_3 VARCHAR(20), 
-    keywords_1 VARCHAR(20), keywords_2 VARCHAR(20), keywords_3 VARCHAR(20)
+    popularity FLOAT, production_countries VARCHAR(200), release_date DATE,
+    revenue BIGINT, runtime FLOAT, spoken_languages VARCHAR(200), vote_average FLOAT, vote_count INTEGER,
+    genres_1 VARCHAR(200), genres_2 VARCHAR(200), genres_3 VARCHAR(200), 
+    keywords_1 VARCHAR(200), keywords_2 VARCHAR(200), keywords_3 VARCHAR(200)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- movie poster
@@ -67,9 +67,9 @@ CREATE TABLE IF NOT EXISTS movies_poster(
 -- korean movie
 CREATE TABLE IF NOT EXISTS movies_korean(
     mid INTEGER, release_year INTEGER, original_title VARCHAR(100),
-    genres VARCHAR(50), director VARCHAR(10), producer VARCHAR(40), writer VARCHAR(20),
-    cast VARCHAR(100), crank_in VARCHAR(50), crank_out VARCHAR(50), presents VARCHAR(50), production VARCHAR(50),
-    release_data VARCHAR(50), etc TEXT
+    genres VARCHAR(100), director VARCHAR(100), producer VARCHAR(100), writer VARCHAR(100),
+    cast TEXT, crank_in VARCHAR(100), crank_out VARCHAR(100), presents VARCHAR(100), distributor VARCHAR(100), production VARCHAR(100),
+    release_data VARCHAR(100), etc TEXT
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- korean movie poster
--- sql_files/drop.sql
+++ sql_files/drop.sql
@@ -7,6 +7,8 @@ drop table movies_disney_plus;
 drop table movies_hulu;
 drop table movies_poster;
 drop table movies_korean;
+drop table movies_kor_poster;
 drop table user_db;
 drop table user_board;
 drop table user_fav_db;
+drop table user_search_db;
