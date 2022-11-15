86efe53a8a534346047766e72e584a406b4f7b21
YujinChang <yujinchang804@ewhain.net>
2022-11-16 04:32:47 +0900

    Feat: modify.php and session

--- basic_php_files/register.php
+++ basic_php_files/register.php
@@ -17,15 +17,20 @@
       $check_res=mysqli_query($mysqli, $check_name_sql);
       if(!empty(mysqli_fetch_array($check_res))){
         echo '<h1>이미 존재합니다.</h1>';
+
       }
       elseif($_POST["_password"]!=$_POST["check_password"]){
         echo '<h1>비밀번호를 확인하세요.</h1>';
+
       }
       else{
         $_SESSION[ 'user_name' ]=$_POST["user_name"];        
         $res=mysqli_query($mysqli, $register_sql);
         echo '<h1>회원가입이 되었습니다.</h1>';
+      
      }
+     header("Location:../Main_Page.php");
+     die();
     ?>
   </body>
 </html>
