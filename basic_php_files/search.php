5ab6c304bb21c0700bbefb9e83ef4f192e5faa1f
YujinChang <yujinchang804@ewhain.net>
2022-11-17 01:53:28 +0900

    Merge branch 'master' of https://github.com/yujinchang00/team19

--- basic_php_files/search.php
+++ basic_php_files/search.php
@@ -1,49 +1,36 @@
 <?php 
   include 'session.php';
+  include "mysql_connect.php";
 
   
+  function save_query($login, $mysql, $query, $user_name) {
+    # (1) user_name에 해당하는 user_id 찾아주기
+    $find_id_sql="select user_id from user_db where user_name ='".(string)$_SESSION["user_name"]."'";
+    $find_id_res=mysqli_query($mysql, $find_id_sql);
+    ### note: session이 있을때에만 이 함수가 쓰이기 때문에 반드시 $user_id가 만들어짐
+    while($id=mysqli_fetch_assoc($find_id_res)) $user_id = $id['user_id'];
 
-  function save_query($login, $mysqli, $query){
-    
-     
-      #$user_id=1;
-      $find_id_sql="select user_id from user_db where user_name ='".(string)$_SESSION["user_name"]."'";
-      $id_res=mysqli_query($mysqli, $find_id_sql);
-      while($id=mysqli_fetch_assoc($id_res)) $user_id=$id["user_id"];
-
-      $user_id=1;
-
-  
-      $find_user_sql="select * from user_search_db where user_id ='".$user_id."';";
+    # (2) user_search_db에서 user_id 찾기
+    $find_user_sql="select * from user_search_db where user_id ='".$user_id."';";
+    $find_user_res=mysqli_query($mysql, $find_user_sql);
+    if(empty(mysqli_fetch_array($find_user_res))) {
       $create_sql="insert into user_search_db (user_id, search1) values (' ".$user_id."','".$query."');";
-     
+      $c_res = mysqli_query($mysql, $create_sql);
+    } else {
       $select_sql="select search1, search2, search3, search4 from user_search_db where user_id='".$user_id."'";
-      $select_res=mysqli_query($mysqli, $select_sql); 
-      $search_list=[];
-
-      while($search=mysqli_fetch_assoc($select_res)) {
-        $search_list[0]=$search["search1"];
-        $search_list[1]=$search["search2"];
-        $search_list[2]=$search["search3"];
-        $search_list[3]=$search["search4"];
-      
+      $select_res=mysqli_query($mysql, $select_sql);
+      while($search=mysqli_fetch_array($select_res, MYSQLI_NUM)) {
+        for ($i = 0; $i < 4; $i++) {
+          if(empty($search[$i])) {break;}
+          $update_sql = "update user_search_db set search".($i+2)." = '".$search[$i]."' where user_id = '".$user_id."'";
+          $update = mysqli_query($mysql, $update_sql);
+        }
+        $update_sql = "update user_search_db set search1 = '".$query."' where user_id = '".$user_id."'";
+        $update = mysqli_query($mysql, $update_sql);
       }
-      for($i=0;$i<4;$i++) if(empty($search_list[$i])) $search_list[$i]=" ";
-
-
-      $update_sql="update user_search_db set search1='".$query."', search2='".$search_list[0]."', search3='".$search_list[1].
-      "', search4='".$search_list[2]."', search5='".$search_list[3]."' where user_id='".$user_id."';";
-      
-      $find_user_res=mysqli_query($mysqli, $find_user_sql);
-      if($login){
-                
-        if(empty(mysqli_fetch_array($find_user_res)))
-            if(!$create_res=mysqli_query($mysqli, $create_sql)) mysqli_error($mysqli) ;
-      
-         else
-            if(!$update_res=mysqli_query($mysqli, $update_sql)) mysqli_error($mysqli) ;      
-      }
-    
-
     }
-?>
+
+
+  }
+
+  ?>
