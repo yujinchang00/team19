1a4ee823567e30ffa8bbfb69c3925251fdce11ff
haesung <ellen1216@naver.com>
2022-11-17 02:09:13 +0900

    community 링크 연결 개선+actor 연결개선

--- Community_Page.php
+++ Community_Page.php
@@ -23,7 +23,7 @@
                 <li><a onclick="location.href='Favor_Page.php'">OTT SERVICE</a></li>
                 <li><a onclick="location.href='Genre_Page.php'">GENRE</a></li>
                 <li><a onclick="location.href='Event_Page_autoscroll.php'">EVENT</a></li>
-                <li><a  class="text_green" onclick="location.href='Community_Page.php'">COMMUNITY</a></li>
+                <li><a  class="text_green" onclick="location.href='Community_Result_Page.php'">COMMUNITY</a></li>
                 <li><a onclick="location.href='Actor_page.php'">KOREAN ACTOR</a></li>
             </ul>
         </nav>
