f34320386eaf9fa5eabe8b426b159b17d27e8167
YujinChang <yujinchang804@ewhain.net>
2022-11-16 23:57:39 +0900

    YuJin: Korean Actor Page 완성

--- basic_php_files/delete.php
+++ basic_php_files/delete.php
@@ -1,9 +1,10 @@
-r<?php 
+<?php 
   include 'session.php';
   include "mysql_connect.php";
-  function delete_all($mysqli, $user_name){
-  $delete_sql="delete from user_search_db where user_search_db.user_id = (select user_id from user_db where user_db.user_name = '".$_SESSION["user_name"]."')";
-      
-  $delete_res=mysqli_query($mysqli, $delete_sql);
+  function delete_all($mysql, $user_name){
+    $delete_sql="delete from user_search_db where user_search_db.user_id = (select user_id from user_db where user_db.user_name = '".$_SESSION["user_name"]."')";
+    $delete_res=mysqli_query($mysql, $delete_sql);
+    header("Location: ../Actor_Page.php");
   }
-    ?>
+  if(isset($_POST['clicked'])) delete_all($mysqli, $_SESSION['user_name']);
+?>
